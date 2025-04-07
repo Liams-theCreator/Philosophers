638a16fccee71d8aaa858585b7700aa2b6d27ed1
Liams-theCreator <mmeellaallii1@gmail.com>
2025-04-07 13:25:23 +0100

    testing

--- main.c
+++ main.c
@@ -6,12 +6,48 @@
 /*   By: imellali <imellali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 16:25:26 by imellali          #+#    #+#             */
-/*   Updated: 2025/04/04 16:27:43 by imellali         ###   ########.fr       */
+/*   Updated: 2025/04/04 17:20:11 by imellali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#include "philo.h" // Suppose you place your structs and function prototypes here
+
+int main(int argc, char **argv)
 {
-    // test
-    return 0;
+    t_config    config;
+    t_philo     *philos;
+    pthread_t   *threads;
+    pthread_mutex_t *forks;
+    int         i;
+
+    if (parse_arguments(argc, argv, &config) == -1)
+        return (EXIT_FAILURE);
+
+    philos = malloc(sizeof(t_philo) * config.number_of_philos);
+    threads = malloc(sizeof(pthread_t) * config.number_of_philos);
+    forks = malloc(sizeof(pthread_mutex_t) * config.number_of_philos);
+
+    for (i = 0; i < config.number_of_philos; i++)
+        pthread_mutex_init(&forks[i], NULL);
+
+    for (i = 0; i < config.number_of_philos; i++)
+    {
+        philos[i].id = i;
+        philos[i].left_fork = &forks[i];
+        philos[i].right_fork = &forks[(i + 1) % config.number_of_philos];
+        philos[i].last_eat_time = 0;
+        philos[i].times_eaten = 0;
+        philos[i].config = &config;
+        pthread_create(&threads[i], NULL, philosopher_routine, &philos[i]);
+    }
+
+    for (i = 0; i < config.number_of_philos; i++)
+        pthread_join(threads[i], NULL);
+
+    for (i = 0; i < config.number_of_philos; i++)
+        pthread_mutex_destroy(&forks[i]);
+    free(forks);
+    free(threads);
+    free(philos);
+    return (0);
 }
---
+++ philo.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imellali <imellali@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/04 16:40:17 by imellali          #+#    #+#             */
+/*   Updated: 2025/04/04 16:45:14 by imellali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+
+typedef struct s_config
+{
+    int number_of_philos; 
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+}   t_config;
+
+typedef struct s_philo
+{
+    int             id;
+    pthread_mutex_t *left_fork;
+    pthread_mutex_t *right_fork;
+    long            last_eat_time;
+    int             times_eaten;
+    t_config        *config;
+}   t_philo;
+
+int   parse_arguments(int argc, char **argv, t_config *config);
+void  *philosopher_routine(void *arg);
+
+#endif
